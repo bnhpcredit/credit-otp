b75a0557ab9eb6c266721ee032e4e3fd7c5107de
ofirt <ofirt@LAPTOP-DREVRBJS.mshome.net>
2021-04-11 08:33:44 +0300

    first commit

--- otp/src/main/java/com/example/otp/services/impl/OtpServiceImpl.java
+++ otp/src/main/java/com/example/otp/services/impl/OtpServiceImpl.java
@@ -16,6 +16,7 @@ public class OtpServiceImpl implements OtpService {
 		int randomPin   =(int) (Math.random()*9000)+1000;
         String otp  = String.valueOf(randomPin);
         return otp;
+        
         }
 
 	@Override
